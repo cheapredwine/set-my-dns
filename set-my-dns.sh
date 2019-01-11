09b195b4799eec8c7fd0168d66ae0cd10468d9f6
Jason Sherron <jason@autonomic.ai>
2019-01-10 19:21:03 -0800

    split out result tests

--- set-my-dns.sh
+++ set-my-dns.sh
@@ -3,11 +3,15 @@
 if [ $1 = "off" ]; then
     echo "resetting to DHCP"
     networksetup -setdnsservers Wi-Fi empty
+    current=$(scutil --dns | grep nameserver)
 else
     echo "setting custom DNS"
     networksetup -setdnsservers Wi-Fi 1.1.1.1 8.8.8.8 8.8.4.4
+    current=$(networksetup -getdnsservers Wi-Fi)
+    echo "new DNS resolvers:" $current
 fi
 
-networksetup -getdnsservers Wi-Fi
-echo "testing:"
-nslookup google.com | grep Server
+echo $current
+
+#networksetup -getdnsservers Wi-Fi
+#nslookup google.com | grep Server
