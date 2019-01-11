83eb4a210a25885b57dc3d4f7236837d8178a120
Jason Sherron <jason@autonomic.ai>
2019-01-10 19:24:06 -0800

    remove extra output

--- set-my-dns.sh
+++ set-my-dns.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 if [ $1 = "off" ]; then
-    echo "resetting to DHCP"
+    #echo "resetting to DHCP"
     networksetup -setdnsservers Wi-Fi empty
     current=$(scutil --dns | grep nameserver)
 else
-    echo "setting custom DNS"
+    #echo "setting custom DNS"
     networksetup -setdnsservers Wi-Fi 1.1.1.1 8.8.8.8 8.8.4.4
     current=$(networksetup -getdnsservers Wi-Fi)
-    echo "new DNS resolvers:" $current
 fi
 
 echo $current
