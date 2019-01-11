ba215c8a39545f6012824aa7690bcc56aa3937cb
Jason Sherron <jason@autonomic.ai>
2019-01-10 19:30:44 -0800

    Add OpenDNS and extract to variable

--- set-my-dns.sh
+++ set-my-dns.sh
@@ -4,13 +4,15 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
+list="1.1.1.1 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220"
+
 if [ $1 = "off" ]; then
     #echo "resetting to DHCP"
     networksetup -setdnsservers Wi-Fi empty
     current=$(scutil --dns | grep nameserver)
 else
     #echo "setting custom DNS"
-    networksetup -setdnsservers Wi-Fi 1.1.1.1 8.8.8.8 8.8.4.4
+    networksetup -setdnsservers Wi-Fi $list
     current=$(networksetup -getdnsservers Wi-Fi)
 fi
 
