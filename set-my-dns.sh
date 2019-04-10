0295f8583e636ad3d6ed850d8193a0a62eae8381
Jason Sherron <jason@autonomic.ai>
2019-04-10 12:30:48 -0400

    rename default to rfc1918

--- set-my-dns.sh
+++ set-my-dns.sh
@@ -7,15 +7,15 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 list="1.1.1.1 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220"
-default="192.168.1.1"
+rfc1918="192.168.1.1"
 
 if [ $1 = "off" ]; then
     #echo "resetting to DHCP"
     networksetup -setdnsservers Wi-Fi empty
     current=$(scutil --dns | grep nameserver)
-elif [ $1 = "default" ]; then
-    #echo "resetting to DHCP"
-    networksetup -setdnsservers Wi-Fi $default
+elif [ $1 = "rfc1918" ]; then
+    #echo "resetting to RFC1918 default gateway"
+    networksetup -setdnsservers Wi-Fi $rfc1918
     current=$(networksetup -getdnsservers Wi-Fi)
 else
     #echo "setting custom DNS"
